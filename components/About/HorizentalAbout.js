511f30816255f2bfa7b7f558261f54262763e340
kiana lotfi <kiana13799@gmil.com>
2024-04-09 17:37:51 +0330

    swiper

--- components/About/HorizentalAbout.js
+++ components/About/HorizentalAbout.js
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react'
+import { Swiper, SwiperSlide } from 'swiper/swiper-react.mjs'
 // eslint-disable-next-line import/no-extraneous-dependencies
 
 import 'swiper/swiper-bundle.css'
